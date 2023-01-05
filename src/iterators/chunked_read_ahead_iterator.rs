e16623403efdbf974ec70494da7c5deb9a4227a6
Nathan Roach <nathan@fulcrumgenomics.com>
2023-01-04 18:30:24 -0700

    Addressing nils first round of comments on tests

--- src/iterators/chunked_read_ahead_iterator.rs
+++ src/iterators/chunked_read_ahead_iterator.rs
@@ -3,6 +3,8 @@ mod tests {
     use rstest::*;
     use std::mem::drop;
     use std::panic;
+    use std::thread::sleep;
+    use std::time::Duration;
 
     #[allow(clippy::no_effect_underscore_binding)]
     #[rstest]
@@ -11,12 +13,104 @@ mod tests {
     #[case(4)]
     #[case(8)]
     #[case(16)] // larger than the inner iterator
+    fn test_wrapping_empty_iter(#[case] _chunk_size: usize) {
+        let test_vec: Vec<usize> = Vec::new();
+
+        let mut chunked_iter = test_vec.iter(); // TODO add into chunked readahead
+        assert_eq!(chunked_iter.next(), None);
+    }
+
+    #[allow(clippy::no_effect_underscore_binding)]
+    #[rstest]
+    #[case(1)] // smallest possible
+    #[case(2)]
+    #[case(4)]
+    #[case(8)]
+    #[case(16)] // larger than the inner iterator
+    fn test_handle_large_iterator_and_low_chunk_size(#[case] _chunk_size: usize) {
+        let test_vec: Vec<usize> = (0..1_000_000).into_iter().collect();
+        let test_vec2 = test_vec.clone();
+
+        let mut regular_iter = test_vec.iter();
+        let mut chunked_iter = test_vec2.iter(); // TODO add into chunked readahead
+
+        loop {
+            let i = regular_iter.next();
+            let j = chunked_iter.next();
+            assert_eq!(i, j);
+            if i.is_none() {
+                assert!(j.is_none());
+                break;
+            }
+        }
+    }
+
+    /// Continuing to read after the enclosed function ends gets more Nones.
     #[test]
+    fn test_low_bound_on_channel_for_blocking() {
+        let test_vec: Vec<usize> = (0..100_000).into_iter().collect();
+
+        let mut chunked_iter = test_vec.iter(); // TODO add into chunked readahead
+        for _ in 0..4 {
+            chunked_iter.next();
+        }
+        drop(chunked_iter);
+        let mut test_iter = vec![0usize, 1, 2, 3, 4, 5, 6, 7, 8, 9].into_iter();
+        for i in 0..20 {
+            let v = test_iter.next();
+            if i < 10 {
+                assert_eq!(v, Some(i));
+            } else {
+                assert_eq!(v, None);
+            }
+        }
+    }
+
+    #[allow(clippy::no_effect_underscore_binding)]
+    #[rstest]
+    #[case(1)] // smallest possible
+    #[case(2)]
+    #[case(4)]
+    #[case(8)]
+    #[case(16)] // larger than the inner iterator
+    fn test_dropping_before_doesnt_explode(#[case] _chunk_size: usize) {
+        let test_vec = vec![0usize, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let chunked_iter = test_vec.iter(); // TODO add into chunked readahead
+        sleep(Duration::from_millis(10));
+        drop(chunked_iter);
+    }
+
+    #[allow(clippy::no_effect_underscore_binding)]
+    #[rstest]
+    #[case(1)] // smallest possible
+    #[case(2)]
+    #[case(4)]
+    #[case(8)]
+    #[case(16)] // larger than the inner iterator
     fn test_dropping_half_used_iterator_doesnt_explode(#[case] _chunk_size: usize) {
         let test_vec = vec![0usize, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
         let mut chunked_iter = test_vec.iter(); // TODO add into chunked readahead
         for _ in 0..4 {
+            chunked_iter.next();
+        }
+        drop(chunked_iter);
+    }
+
+    #[allow(clippy::no_effect_underscore_binding)]
+    #[rstest]
+    #[case(1)] // smallest possible
+    #[case(2)]
+    #[case(4)]
+    #[case(8)]
+    #[case(16)] // larger than the inner iterator
+    fn test_dropping_fully_used_iterator_doesnt_explode(#[case] _chunk_size: usize) {
+        let test_vec = vec![0usize, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let mut chunked_iter = test_vec.iter(); // TODO add into chunked readahead
+                                                // need to do it this way so we don't lose ownership
+        for _ in 0..test_vec.len() {
             chunked_iter.next();
         }
         drop(chunked_iter);
@@ -41,7 +135,21 @@ mod tests {
             let j = chunked_iter.next();
             assert_eq!(i, j);
             if i.is_none() {
+                assert!(j.is_none());
                 break;
+            }
+        }
+    }
+
+    #[test]
+    fn test_read_past_end() {
+        let mut test_iter = vec![0usize, 1, 2, 3, 4, 5, 6, 7, 8, 9].into_iter();
+        for i in 0..20 {
+            let v = test_iter.next();
+            if i < 10 {
+                assert_eq!(v, Some(i));
+            } else {
+                assert_eq!(v, None);
             }
         }
     }
