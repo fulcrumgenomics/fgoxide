cc9e7888b3ff0e3c58959632232bd67afd588e06
jdidion <github@didion.net>
2023-11-07 13:12:36 -0800

    formatting

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -47,7 +47,7 @@ use std::marker::PhantomData;
 use std::path::Path;
 
 use crate::{FgError, Result};
-use csv::{QuoteStyle, ReaderBuilder, DeserializeRecordsIntoIter, WriterBuilder, Writer};
+use csv::{DeserializeRecordsIntoIter, QuoteStyle, ReaderBuilder, Writer, WriterBuilder};
 use flate2::bufread::MultiGzDecoder;
 use flate2::write::GzEncoder;
 use flate2::Compression;
